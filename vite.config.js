ab7af2d1534b4253e08ba9ab3c8060697dd152d1
Dicky Salim <dickysal1506@gmail.com>
2026-05-01 00:31:22 +0700

    Habe Fixed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // Redirect 404s to index.html so react-router-dom handles all paths
-    historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
   },
 })
